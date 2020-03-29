3f51d0c41d07441760e37c9d67fadbc91594c14f
Daniel Brai <daniel.gm.brai@gmail.com>
2020-03-29 10:42:20 -0300

    Templates, Direticas Estruturais e Interpolacao

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `
-  <div>
-    <h1>{{pageTitle}}</h1>
-    <div>
-      <p>My first component</p>
-    </div>
-  </div>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
