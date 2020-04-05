7712c112e3db58641530ba424e9fcb0438bff47a
Daniel Brai <danielgm@ciandt.com>
2020-04-04 22:39:14 -0300

    Modulos

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,36 +1,26 @@
-import { ProductListComponent } from './products/product-list-component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
-import { StarRatingComponent } from './shared/star-rating/star-rating.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacePipe,
-    StarRatingComponent,
-    ProductDetailComponent,
     WelcomeComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: ' welcome', pathMatch: 'full' },
       { path: '**', redirectTo: ' welcome', pathMatch: 'full' },
     ]),
+    ProductsModule,
   ],
   bootstrap: [AppComponent],
 })
